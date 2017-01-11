f50fd0c825d14a3715501acc2ee5ca34eedb709e
startprogress <860313haobo@gmail.com>
2017-01-11 22:37:03 +0800

    1，加入少量注释 2，防止路径最后不带/ 3，改回默认pipelines.py

--- ccqsc/spiders/cspider.py
+++ ccqsc/spiders/cspider.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
 import os
 import scrapy
 import datetime
@@ -9,8 +11,11 @@ from scrapy.http.request import Request
 
 class ccqsc(scrapy.Spider):
     name = "ccqsc"
+    #如果不提供参数，日期默认为系统当前日期，路径默认为"data/"
     today =  datetime.datetime.now().strftime("%Y-%m-%d")
     def __init__(self, date=today, path='data/'):
+        if path[-1] != '/':
+            path = path + '/'
         self.date = date
         self.path = path
 
@@ -18,9 +23,9 @@ class ccqsc(scrapy.Spider):
     start_urls = [
         "http://www.cottoneasy.com/storage/maStorageProgressListInit"
     ]
-
+    #第一层，拿到42个储运站的URL
     def parse(self, response):
-        #item = tmpItem()
+        # 将时间范围拆分放入list中
         if len(self.date) > 10:
             date_range = []
             date1 = self.date[0:10]
@@ -40,16 +45,17 @@ class ccqsc(scrapy.Spider):
                     '//a[@class="fsize_12 btn-link pr_5"]')[i].xpath('@href').extract())
                 req = "http://www.cottoneasy.com/" + originreq[:-10] + date_i
                 yield Request(req, callback=self.parse2)
-
+    #第二层，拿到每一批次的编号
     def parse2(self, response):
         item = tmpItem()
+        #只要批次号，所以每8个td才取一次
         for i in range(8, len(response.xpath('//td')), 8):
             batchnum = "".join(response.xpath(
                 '//td')[i].xpath('text()').extract())
             req = "http://www.ccqsc.gov.cn/query/compareBatchInfoData.action?batchCodeInput=" + batchnum
             item['batch'] = batchnum
             yield Request(req, meta={'item': item, 'batchnum': batchnum}, callback=self.parse3)
-
+    #第三层，依据编号到ccsqc网站拿json数据存入
     def parse3(self, response):
         item = response.meta['item']
         batchnum = response.meta['batchnum']
