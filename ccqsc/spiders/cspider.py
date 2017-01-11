8ac2e18bf0b2f836dab7084fb686a8d617249471
startprogress <860313haobo@gmail.com>
2017-01-11 17:31:24 +0800

    增加对时间段的支持YYYY-MM-DD~YYYY-MM-DD

--- ccqsc/spiders/cspider.py
+++ ccqsc/spiders/cspider.py
@@ -21,11 +21,25 @@ class ccqsc(scrapy.Spider):
 
     def parse(self, response):
         #item = tmpItem()
-        for i in range(0, len(response.xpath('//a[@class="fsize_12 btn-link pr_5"]')), 7):
-            originreq = "".join(response.xpath(
-                '//a[@class="fsize_12 btn-link pr_5"]')[i].xpath('@href').extract())
-            req = "http://www.cottoneasy.com/" + originreq[:-10] + self.date
-            yield Request(req, callback=self.parse2)
+        if len(self.date) > 10:
+            date_range = []
+            date1 = self.date[0:10]
+            date2 = self.date[11:]
+            curr_date = datetime.date(
+                int(date1[0:4]), int(date1[5:7]), int(date1[8:10]))
+            end_date = datetime.date(
+                int(date2[0:4]), int(date2[5:7]), int(date2[8:10]))
+            while curr_date != end_date:
+                date_range.append(str(curr_date))
+                curr_date += datetime.timedelta(days=1)
+        else:
+            date_range = [self.date]
+        for date_i in date_range:
+            for i in range(0, len(response.xpath('//a[@class="fsize_12 btn-link pr_5"]')), 7):
+                originreq = "".join(response.xpath(
+                    '//a[@class="fsize_12 btn-link pr_5"]')[i].xpath('@href').extract())
+                req = "http://www.cottoneasy.com/" + originreq[:-10] + date_i
+                yield Request(req, callback=self.parse2)
 
     def parse2(self, response):
         item = tmpItem()
